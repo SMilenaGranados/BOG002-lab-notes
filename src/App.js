b11158ed4ed8df81760a737e292566268d634e26
SMilena <sandramilenagc8@gmail.com>
2021-07-30 22:32:54 -0500

    Hook useForm para registro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from "react";
-import firebase from "./configuration/Firebase";
-import "firebase/auth";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./routes/Login";
 import Register from "./routes/Register";
 import Notes from "./routes/Notes";
 
 function App() {
-
-  firebase.auth().onAuthStateChanged(user => {
-    console.log(user);
-  })
-
   return (
     <Router>
       <div className="App">
         <Switch>
-        <Route path="/" exact>
+          <Route path="/" exact>
             <Login />
           </Route>
           <Route path="/register">
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,23 +1,78 @@
-import React from "react";
-import logoAllNotes from '../assets/LogoAllNotes.png';
-
+import React, { useEffect } from "react";
+import logoAllNotes from "../assets/LogoAllNotes.png";
+import { useForm } from "../hooks/useForm";
 
 const Register = () => {
-    return (
-        <section>
-        <div>
-            <img src={logoAllNotes} alt='logo' />
-        </div>
-        <form>
-            <input type="email" placeholder="email" class="input" required></input>
-            <p>Mensaje de error</p>
-            <input type="password" placeholder="password" class="input" required></input>
-            <p>Mensaje de error</p>
-            <button type="submit" class="button">Register</button>
-            
-        </form>
-        </section>
-    )
-}
+  const [formValues, handleInputChange ] = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export default Register
+  const { name, email, password } = formValues;
+
+  useEffect( () => {
+      console.log("email cambio")
+  }, [ password ]);
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+      console.log( formValues );
+
+  }
+
+  return (
+    <section>
+      <div>
+        <img src={logoAllNotes} alt="logo" />
+      </div>
+
+      <form onSubmit={ handleSubmit }>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            placeholder="user name"
+            value={name}
+            onChange={handleInputChange}
+            required
+          ></input>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={handleInputChange}
+            required
+          ></input>
+        </div>
+        <p>Mensaje de error</p>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={handleInputChange}
+            required
+          ></input>
+        </div>
+        <p>Mensaje de error</p>
+        <button type="submit" class="button">
+          Register
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Register;
+
+// firebase.auth().onAuthStateChanged(user => {
+//     console.log(user);
+//   })
