7fe862f0426008b0035fcaea382318f67a029f0f
SMilena <sandramilenagc8@gmail.com>
2021-08-01 18:03:12 -0500

    Estilos pantalla Notes

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -99,6 +99,7 @@ const Login = () => {
         </Link>
       </div>
     </section>
+    
   );
 };
 
--- src/routes/Notes.jsx
+++ src/routes/Notes.jsx
@@ -1,12 +1,36 @@
 import React from "react";
-import "../styles/notes.css"
+import "../styles/notes.css";
+import logoAllNotes from "../assets/LogoAllNotes.png";
+import pencil from "../assets/Pencil.png";
+import line from "../assets/Line.png";
 
 const Notes = () => {
-    return (
+  return (
+    <section id="sectionNotes">
+      <header id="headerNotes">
         <div>
-            <h1>Screen Notes</h1>
+          <img id="logoAllNotesNot" src={logoAllNotes} alt="logo" />
         </div>
-    )
-}
+        <p id="HiUser">Hi! milena granados</p>
 
-export default Notes
+        <button id="buttonAddNote" type="button">
+          Add your new note
+          <img id="logoPencil" src={pencil} alt="logoPencil" />
+        </button>
+
+        <button id="buttonLogOut" type="button" class="button">
+          Log Out
+        </button>
+      </header>
+      <div>
+        <img id="line" src={line} alt="line"></img>
+      </div>
+
+      <div>
+        <p>Aqui van las notas</p>
+      </div>
+    </section>
+  );
+};
+
+export default Notes;
